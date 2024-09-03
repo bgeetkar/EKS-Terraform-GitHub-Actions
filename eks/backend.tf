34c0c54ffc3ca8337dd3e023e23c1f9d520b7b4b
Deepak Geetkar <141703246+bgeetkar@users.noreply.github.com>
2024-09-03 22:38:46 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "mern-application-platform"
-    region         = "eu-west-1"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
